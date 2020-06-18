0e4871fac2f23226c2bea048dde596edf4ff7f32
Omarjabaly <60774903+Omarjabaly@users.noreply.github.com>
2020-06-18 15:32:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import 'tachyons';
-import './App.css';
 import NumberFact from './NumberFact';
 
 class App extends Component {
@@ -91,22 +90,22 @@ class App extends Component {
         
         <h1 className='tc f3 dark-blue'> Trivia
             <input type='number' className='w5 mh4' placeholder='Enter Number' id='triviaInput' />
-            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue" onClick={this.getTrivia}>Go</p>
+            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue pointer" onClick={this.getTrivia}>Go</p>
         </h1>
         
         <h1 className='tc f3 dark-blue'> Math 
             <input type='number' className='w5 mh4' placeholder='Enter Number' id='mathInput' />
-            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue" onClick={this.getMath}>Go</p>
+            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue pointer" onClick={this.getMath}>Go</p>
         </h1>
         
         <h1 className='tc f3 dark-blue'> Date 
             <input className='w5 mh4 gray' type='text' placeholder='Format MM/DD' id='dateInput' />
-            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue" onClick={this.getDate}>Go</p>
+            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue pointer" onClick={this.getDate}>Go</p>
         </h1>
         
         <h1 className='tc f3 dark-blue'> Year 
             <input className='w5 mh4' type='year' placeholder='Enter Year' id='yearInput' />
-            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue" onClick={this.getYear}>Go</p>
+            <p className="f5 grow br-pill ba bw2 ph3 pv1 mb1 dib dark-blue pointer" onClick={this.getYear}>Go</p>
         </h1>
 
         <NumberFact 
