947ea31e36356c9f4116b29727f45864e9436010
Omarjabaly <60774903+Omarjabaly@users.noreply.github.com>
2020-06-18 13:54:31 +0300

    Updates

---
+++ precache-manifest.547d522cca1717ed8d34ef6a840f90f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f0dc04c13ca5109c87d1f7b7fb0aea",
+    "url": "/numberstalk/index.html"
+  },
+  {
+    "revision": "920c94ced65b6a332ba4",
+    "url": "/numberstalk/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be61fed08188a2fccb72",
+    "url": "/numberstalk/static/css/main.6edd400e.chunk.css"
+  },
+  {
+    "revision": "920c94ced65b6a332ba4",
+    "url": "/numberstalk/static/js/2.e4d2e11c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numberstalk/static/js/2.e4d2e11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be61fed08188a2fccb72",
+    "url": "/numberstalk/static/js/main.e4d6eb51.chunk.js"
+  },
+  {
+    "revision": "c04704a310e76161f360",
+    "url": "/numberstalk/static/js/runtime-main.a454accd.js"
+  },
+  {
+    "revision": "5d16b8a265a3a4adf564a8e4e32f6ee9",
+    "url": "/numberstalk/static/media/Amsterdam.5d16b8a2.ttf"
+  }
+]);
